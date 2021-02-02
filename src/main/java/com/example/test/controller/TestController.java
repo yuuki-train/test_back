092181b07cd84fcc3461729d76c297213f2c6646
yuuki <yuuki323-sld51@kcf.biglobe.ne.jp>
2021-02-02 17:06:33 +0900

    test_back modified 0202

--- src/main/java/com/example/test/controller/TestController.java
+++ src/main/java/com/example/test/controller/TestController.java
@@ -1,7 +1,5 @@
 package com.example.test.controller;
 
-import com.example.test.entity.Result;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -10,52 +8,28 @@ import java.util.Map;
 @CrossOrigin(origins ="*")
 public class TestController {
 
-    @RequestMapping(value ="/getResult", method = {RequestMethod.GET})
+    @RequestMapping(value ="/plus", method = {RequestMethod.POST})
+    @ResponseBody
     public String plus(@RequestParam Map<String, String> requestParams){
 
-        /*
+        String result;
+
         try{
-            /*
             String strLeft = requestParams.get("leftNumber");
             String strRight = requestParams.get("rightNumber");
 
             int left = Integer.parseInt(strLeft);
             int right =Integer.parseInt(strRight);
             int total= left + right;
-             */
+            result = Integer.toString(total);
 
-            return "[{\"left\": 1, \"right\": 1, \"total\": 2}]";
-
-        /*
-        }catch (NumberFormatException || NullPointerException e1){
-            e1.printStackTrace();
+        }catch(NumberFormatException e1){
             result = "error";
         }
-        */
+        String response =  "[{ \"result\": " + result + "}]";
+        System.out.println(response);
+        return response;
     }
 
-    /*
-    @RequestMapping(value="/getResult", method = RequestMethod.GET)
-    @ResponseBody
-    public String getResult(){
-        int left = getLeft();
-        int right = getRight();
-        int total = getTotal();
-        System.out.println(left);
-        System.out.println(right);
-        System.out.println(total);
-        String strLeft = Integer.toString(left);
-        String strRight = Integer.toString(right);
-        String strTotal = Integer.toString(total);
-
-
-        String json = "[{\"left\": " + strLeft +
-                ", \"right\": " + strRight +
-                ", \"total\": " + strTotal +
-                "}]";
-        return json;
-    }
-
-         */
 }
 
